f6ce5acea0ae56c608806a836eba659a9a636cda
ser-2007 <sersah95@gmail.com>
2021-06-30 16:57:53 +0300

    Update app.py

--- python/hands-on/flask-04-handling-forms-POST-GET-Methods/Flask_Get_Post_Method/app.py
+++ python/hands-on/flask-04-handling-forms-POST-GET-Methods/Flask_Get_Post_Method/app.py
@@ -22,4 +22,5 @@ def total():
         return render_template("number.html")
 # Add a statement to run the Flask application which can be debugged.
 if __name__=="__main__":
-    app.run(debug=True)
+    #app.run(debug=True)
+    app.run(host='0.0.0.0', port=80)
